fb5db97b0313af811a6eb369ee619321cc844944
hamiddcivil <95771579+hamiddcivil@users.noreply.github.com>
2023-07-23 00:40:29 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1b87b8cce001fcbd4c5868686e88bfe13a419450";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
